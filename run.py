a762f161e60da22cda91e7ca0e907f31ffc497b6
ironarmchad <mcline@muralab.org>
2019-08-08 19:31:44 -0400

    reverting the third change

--- run.py
+++ run.py
@@ -1,13 +1,12 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == "__main__":
-    char_app = create_app('prod')
-    with char_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(user_name='su_ironarmchad').first():
-            User.create_user(user='su_ironarmchad',
-                            password='PIANO@230jap',
-                            role='SUPER')
+char_app = create_app('prod')
+with char_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='su_ironarmchad').first():
+        User.create_user(user='su_ironarmchad',
+                        password='PIANO@230jap',
+                        role='SUPER')
 
-    char_app.run()
+char_app.run()
