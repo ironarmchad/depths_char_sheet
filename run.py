4ae849d148957f8074fc069290ae34f2287a1efb
ironarmchad <mcline@muralab.org>
2019-08-08 20:09:38 -0400

    more tiny changes

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 from app import create_app, db
 from app.auth.models import User
 
-char_app = create_app('prod')
-with char_app.app_context():
-    db.create_all()
-    if not User.query.filter_by(user_name='su_ironarmchad').first():
-        User.create_user(user='su_ironarmchad',
-                        password='PIANO@230jap',
-                        role='SUPER')
 
-char_app.run()
+if __name__ == '__main__':
+    char_app = create_app('prod')
+    with char_app.app_context():
+        db.create_all()
+        if not User.query.filter_by(user_name='su_ironarmchad').first():
+            User.create_user(user='su_ironarmchad',
+                            password='PIANO@230jap',
+                            role='SUPER')
+
+    char_app.run()
+else:
+    char_app= create_app('prod')
+    with char_app.app_context():
+        db.create_all()
+    char_app.run()
