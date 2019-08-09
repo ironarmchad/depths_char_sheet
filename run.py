3299390c1c76cf488c2370bea00e1427fddccfb3
ironarmchad <mcline@muralab.org>
2019-08-09 10:00:42 -0400

    python run.py now refers to correct config

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import sys
 
 
 if __name__ == '__main__':
-    char_app = create_app('prod')
+    char_app = create_app('dev')
     with char_app.app_context():
         db.create_all()
         if not User.query.filter_by(user_name='su_ironarmchad').first():
