d963560a503c1ed41fded902068deeb592a7bdf3
ironarmchad <mcline@muralab.org>
2019-08-09 08:02:01 -0400

    fiddling 13

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from app.auth.models import User
+import sys
 
 
 if __name__ == '__main__':
@@ -15,11 +16,17 @@ if __name__ == '__main__':
 
 else:
     char_app = create_app('prod')
+    sys.stderr.write('check 1\n')
     with char_app.app_context():
+        sys.stderr.write('check 2\n')
         db.create_all()
+        sys.stderr.write('check 3\n')
         if not User.query.filter_by(user_name='su_ironarmchad').first():
+            sys.stderr.write('check 4\n')
             User.create_user(user='su_ironarmchad',
                              password='PIANO@230jap',
                              role='SUPER')
+    sys.stderr.write('check 5\n')
 
     char_app.run()
+    sys.stderr.write('check 6\n')
